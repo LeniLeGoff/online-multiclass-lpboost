b72d60798d889d9ce10c65cc8ab6d818a2ea5410
Léni Le Goff <lenilegoff@gmail.com>
2016-06-28 11:57:29 +0200

    bug fix

--- src/online_rf.h
+++ src/online_rf.h
@@ -146,7 +146,7 @@ public:
 
     ~OnlineRF() {
         for (int nTree = 0; nTree < hp_t::numTrees; nTree++) {
-            delete m_trees[nTree];
+            m_trees[nTree].reset();
         }
     }
 
@@ -160,6 +160,143 @@ protected:
 
     vector< typename OnlineTree<hp_t>::Ptr> m_trees;
 };
+
+template <typename hp_t>
+inline void OnlineNode<hp_t>::update(const Sample& sample) {
+    m_counter += sample.w;
+    m_labelStats(sample.y) += sample.w;
+
+    if (m_isLeaf) {
+        // Update online tests
+        for (vector<RandomTest*>::iterator itr = m_onlineTests.begin(); itr != m_onlineTests.end(); ++itr) {
+            (*itr)->update(sample);
+        }
+
+        // Update the label
+        m_labelStats.maxCoeff(&m_label);
+
+        // Decide for split
+        if (shouldISplit()) {
+            m_isLeaf = false;
+
+            // Find the best online test
+            int nTest = 0, minIndex = 0;
+            double minScore = 1, score;
+            for (vector<RandomTest*>::const_iterator itr(m_onlineTests.begin()); itr != m_onlineTests.end(); ++itr, nTest++) {
+                score = (*itr)->score();
+                if (score < minScore) {
+                    minScore = score;
+                    minIndex = nTest;
+                }
+            }
+            m_bestTest = m_onlineTests[minIndex];
+            for (int nTest = 0; nTest < hp_t::numRandomTests; nTest++) {
+                if (minIndex != nTest) {
+                    delete m_onlineTests[nTest];
+                }
+            }
+
+            // Split
+            pair<VectorXd, VectorXd> parentStats = m_bestTest->getStats();
+            m_rightChildNode = new OnlineNode<hp_t>(*m_numClasses, m_minFeatRange->rows(), *m_minFeatRange, *m_maxFeatRange, m_depth + 1,
+                                              parentStats.first);
+            m_leftChildNode = new OnlineNode<hp_t>(*m_numClasses, m_minFeatRange->rows(), *m_minFeatRange, *m_maxFeatRange, m_depth + 1,
+                                             parentStats.second);
+        }
+    } else {
+        if (m_bestTest->eval(sample)) {
+            m_rightChildNode->update(sample);
+        } else {
+            m_leftChildNode->update(sample);
+        }
+    }
+}
+
+template <typename hp_t>
+inline void OnlineNode<hp_t>::eval(const Sample& sample, Result& result) {
+    if (m_isLeaf) {
+        if (m_counter + m_parentCounter) {
+            result.confidence = m_labelStats / (m_counter + m_parentCounter);
+            result.prediction = m_label;
+        } else {
+            result.confidence = VectorXd::Constant(m_labelStats.rows(), 1.0 / *m_numClasses);
+            result.prediction = 0;
+        }
+    } else {
+        if (m_bestTest->eval(sample)) {
+            m_rightChildNode->eval(sample, result);
+        } else {
+            m_leftChildNode->eval(sample, result);
+        }
+    }
+}
+
+template <typename hp_t>
+inline bool OnlineNode<hp_t>::shouldISplit() const {
+    bool isPure = false;
+    for (int nClass = 0; nClass < *m_numClasses; nClass++) {
+        if (m_labelStats(nClass) == m_counter + m_parentCounter) {
+            isPure = true;
+            break;
+        }
+    }
+
+    if ((isPure) || (m_depth >= hp_t::maxDepth) || (m_counter < hp_t::counterThreshold)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+template <typename hp_t>
+inline void OnlineTree<hp_t>::update(Sample& sample) {
+    m_rootNode->update(sample);
+}
+
+template <typename hp_t>
+inline void OnlineTree<hp_t>::eval(Sample& sample, Result& result) {
+    m_rootNode->eval(sample, result);
+}
+
+template <typename hp_t>
+inline void OnlineRF<hp_t>::update(Sample& sample) {
+    m_counter += sample.w;
+
+    Result result(*m_numClasses), treeResult;
+
+    int numTries;
+    for (int nTree = 0; nTree < hp_t::numTrees; nTree++) {
+        numTries = poisson(1.0);
+        if (numTries) {
+            for (int nTry = 0; nTry < numTries; nTry++) {
+                m_trees[nTree]->update(sample);
+            }
+        } else {
+            m_trees[nTree]->eval(sample, treeResult);
+            result.confidence += treeResult.confidence;
+        }
+    }
+
+    int pre;
+    result.confidence.maxCoeff(&pre);
+    if (pre != sample.y) {
+        m_oobe += sample.w;
+    }
+}
+
+template <typename hp_t>
+inline void OnlineRF<hp_t>::eval(Sample& sample, Result& result) {
+    Result treeResult;
+    for (int nTree = 0; nTree < hp_t::numTrees; nTree++) {
+        m_trees[nTree]->eval(sample, treeResult);
+        result.confidence += treeResult.confidence;
+    }
+
+    result.confidence /= hp_t::numTrees;
+    result.confidence.maxCoeff(&result.prediction);
+}
+
 }
 
 #endif /* ONLINERF_H_ */
